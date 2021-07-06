ac5892e26d1e97cc6eaf93ca888e351c8239934c
zar-wtag <zahidur.rahman@welldev.io>
2021-07-07 01:47:39 +0600

    Form dependency injection.

--- web/modules/custom/rsvplist/src/Form/RSVPForm.php
+++ web/modules/custom/rsvplist/src/Form/RSVPForm.php
@@ -10,11 +10,34 @@ use Drupal\Core\Database\Database;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\node\NodeInterface;
-
+use Drupal\Component\Utility\EmailValidator;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 /**
  * Provides an RSVP Email form.
  */
 class RSVPForm extends FormBase {
+  /**
+   * @var EmailValidator $emailValidator
+   */
+  protected $emailValidator;
+
+  /**
+   * (@inheritDoc)
+   */
+  public function __construct(EmailValidator $emailValidator) {
+    $this->emailValidator = $emailValidator;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    // Instantiates this form class.
+    return new static(
+      // Load the service required to construct this class.
+      $container->get('email.validator')
+    );
+  }
 
   /**
    * (@inheritDoc)
@@ -58,7 +81,7 @@ class RSVPForm extends FormBase {
 
     // \Drupal::service() is not really handy for investigating isValid member function.
     // Let's use dependency injection to get it more portable and also debug friendly.
-    if ($value == !\Drupal::service('email.validator')->isValid($value)) {
+    if ($value == !$this->emailValidator->isValid($value)) {
       $form_state->setErrorByName('email', t('The email address %mail is not valid.', ['%mail' => $value]));
     }
   }
